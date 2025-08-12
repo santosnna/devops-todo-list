82587b0ca07dd641afdfacba296f45c166948c5a
santos.nna <santos.nna@outlook.com>
2025-08-12 21:55:39 +0200

    feat(models): added Task schema.

---
+++ src/models/Task.js
@@ -0,0 +1,25 @@
+// Task.js
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'in-progress', 'completed'],
+    default: 'pending'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
